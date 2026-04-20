e656e170b2ad3486934603e765570649b5d77894
Zakerberg <blmichael1129@icloud.com>
2026-04-20 11:10:19 +0800

    fix:添加了几行代码

--- main.py
+++ main.py
@@ -20,4 +20,6 @@ else:
     print_hi('PyCharm')
     print_hi('PyCharm')
     print_hi('PyCharm')
+    print_hi('PyCharm')
+    print_hi('PyCharm')
 # 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
